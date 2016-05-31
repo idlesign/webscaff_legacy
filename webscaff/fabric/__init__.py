77f0bac1697b81cb720355a21033a69f1f185e12
idle sign <idlesign@yandex.ru>
2016-05-31 21:05:11 +0600

    Defined some shortcuts .

--- webscaff/fabric/__init__.py
+++ webscaff/fabric/__init__.py
@@ -1,3 +1,9 @@
 from . import sys
 from . import project
 from . import django
+
+# Shortcuts.
+from .project.utils import update, dump
+from .sys.utils import status
+from .sys.pip import upgrade_req
+from .sys.apt import upgrade_os
