b770dd20f1fa79fc554be7cd48f758227a899769
idle sign <idlesign@yandex.ru>
2016-06-01 14:03:25 +0600

    venv.create() now perfoms existence check.

--- webscaff/fabric/sys/venv.py
+++ webscaff/fabric/sys/venv.py
@@ -2,6 +2,7 @@ from os import path
 from contextlib import contextmanager
 
 from fabric.api import cd, prefix, task, run
+from fabric.contrib.files import exists
 
 from ..settings import PATH_REMOTE_PROJECT_BASE, PATH_REMOTE_PROJECT
 
@@ -20,7 +21,13 @@ def venv():
 
 @task
 def create(python_path):
-    """Creates virtual environment using given Python interpreter path."""
+    """Creates virtual environment using given Python interpreter path
+    if not already created.
+
+    """
+    if exists(path.join(PATH_REMOTE_PROJECT_BASE, NAME_VIRTUALENV_DIR)):
+        return
+
     with cd(PATH_REMOTE_PROJECT_BASE):
         # Call as module in case `virtualenv` app is not [yet] available.
         run('python -m virtualenv -p %s %s/' % (python_path, NAME_VIRTUALENV_DIR))
