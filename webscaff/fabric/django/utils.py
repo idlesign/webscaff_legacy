b42a8ced4f7ed5295b383bd393123803c3ebfa9b
idle sign <idlesign@yandex.ru>
2016-06-01 15:56:08 +0600

    django.utils.put_settings() now accept reload_uwsgi param.

--- webscaff/fabric/django/utils.py
+++ webscaff/fabric/django/utils.py
@@ -1,6 +1,6 @@
 from os import path
 
-from fabric.api import task, sudo, put as fabric_put
+from fabric.api import task, put as fabric_put
 
 from ..sys.uwsgi import reload_touch
 from ..sys.fs import create_dir, rm
@@ -9,13 +9,14 @@ from ..utils import get_paths
 
 
 @task
-def put_settings():
+def put_settings(reload_uwsgi=True):
     """Puts Django production settings file to remote and reloads uwsgi."""
     # todo set DEBUG=False automatically
     path_local, path_remote = get_paths('%s/settings/prod.py' % PROJECT_NAME)
     create_dir(path.dirname(path_remote))
     fabric_put(path_local, path_remote)
-    reload_touch()
+
+    reload_uwsgi and reload_touch()
 
 
 @task
