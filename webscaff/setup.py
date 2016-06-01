84ddcdab030bc789eeca7224d51c9d243b4fa998
idle sign <idlesign@yandex.ru>
2016-06-01 14:02:24 +0600

    Added basic setup.py for project.

---
+++ webscaff/setup.py
@@ -0,0 +1,9 @@
+"""This is a minimal setup file to allow project installation in development mode via:
+
+    $ pip install -e .
+
+"""
+
+from setuptools import setup
+
+setup(name='myproject', packages=['myproject'])
