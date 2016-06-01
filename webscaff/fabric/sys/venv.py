4fb759da7077c8802eb75fbe4b0c0289bfa7b6c4
idle sign <idlesign@yandex.ru>
2016-06-01 16:01:30 +0600

    sys.venv() now won't cd into project path.

--- webscaff/fabric/sys/venv.py
+++ webscaff/fabric/sys/venv.py
@@ -14,9 +14,8 @@ PATH_VENV = path.join(PATH_REMOTE_PROJECT_BASE, NAME_VIRTUALENV_DIR)
 @contextmanager
 def venv():
     """Temporarily switches into virtual environment."""
-    with cd(PATH_REMOTE_PROJECT):
-        with prefix('. %s/bin/activate' % PATH_VENV):
-            yield
+    with prefix('. %s/bin/activate' % PATH_VENV):
+        yield
 
 
 @task
