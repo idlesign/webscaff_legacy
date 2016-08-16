0935206fbff60dfb75908125a0d6db5a11f647e4
idle sign <idlesign@yandex.ru>
2016-08-16 19:31:10 +0700

    Swap utils added.

--- webscaff/fabric/sys/utils.py
+++ webscaff/fabric/sys/utils.py
@@ -37,3 +37,27 @@ def update_certs():
     """Updates Let's encrypt SSL certificates."""
     with nginx_stopped():
         certs_update()
+
+
+@task
+def swap_make():
+    """Creates a swap file."""
+    swap_file = '/swapfile'
+    sudo('dd if=/dev/zero of=%s bs=1024 count=524288' % swap_file)
+    sudo('chmod 600 %s' % swap_file)
+    sudo('mkswap %s' % swap_file)
+    swap_on()
+
+
+@task
+def swap_on():
+    """Turns on swap."""
+    swap_file = '/swapfile'
+    sudo('swapon %s' % swap_file)
+
+
+@task
+def swap_off():
+    """Turns off swap."""
+    swap_file = '/swapfile'
+    sudo('swapoff %s' % swap_file)
