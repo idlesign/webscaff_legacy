7f75b981b57a94a60e854c487852694c5fc93b95
idle sign <idlesign@yandex.ru>
2016-09-21 20:49:02 +0700

    django.utils: drop_cache_fs -> cache_fs_drop. Added cache_fs_init().

--- webscaff/fabric/django/utils.py
+++ webscaff/fabric/django/utils.py
@@ -3,9 +3,12 @@ from os import path
 from fabric.api import task, put as fabric_put
 
 from ..sys.uwsgi import reload_touch
-from ..sys.fs import create_dir, rm
+from ..sys.fs import create_dir, rm, chmod
 from ..settings import PROJECT_NAME
 from ..utils import get_paths
+
+
+DIR_CACHE = '/tmp/django_cache/'
 
 
 @task
@@ -20,6 +23,16 @@ def put_settings(reload_uwsgi=True):
 
 
 @task
-def drop_cache_fs():
+def cache_fs_drop():
     """Drops Django filesystem cache."""
-    rm('/tmp/django_cache/*', use_local=False)
+    rm('%s*' % DIR_CACHE, use_local=False)
+
+
+@task
+def cache_fs_init():
+    """Initializes django fs cache directory"""
+    rm(DIR_CACHE, use_local=False)
+
+    create_dir(DIR_CACHE)
+    # Accessed both as project user and webserver
+    chmod(DIR_CACHE, 770)
