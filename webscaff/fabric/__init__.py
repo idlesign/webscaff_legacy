346a6a2776ad0d1fbb94052c96d9a366fa331f6c
idle sign <idlesign@yandex.ru>
2016-10-29 21:25:48 +0700

    maintenance function moved to upper level

--- webscaff/fabric/__init__.py
+++ webscaff/fabric/__init__.py
@@ -7,3 +7,4 @@ from .project.utils import update, dump, dj_manage
 from .sys.utils import status
 from .sys.pip import upgrade_req
 from .sys.apt import upgrade_os
+from .sys.nginx import maintenance_off, maintenance_on
