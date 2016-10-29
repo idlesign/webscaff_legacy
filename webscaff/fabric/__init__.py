3a841d0033c7a76603df1678c10a43c31da1c0fc
idle sign <idlesign@yandex.ru>
2016-10-29 21:32:47 +0700

    reboot() function moved to upper level

--- webscaff/fabric/__init__.py
+++ webscaff/fabric/__init__.py
@@ -4,7 +4,7 @@ from . import django
 
 # Shortcuts.
 from .project.utils import update, dump, dj_manage
-from .sys.utils import status
+from .sys.utils import status, reboot
 from .sys.pip import upgrade_req
 from .sys.apt import upgrade_os
 from .sys.nginx import maintenance_off, maintenance_on
