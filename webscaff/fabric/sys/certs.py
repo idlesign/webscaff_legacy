effc3a21428b0855469b8771f75fe5508926dfd5
idle sign <idlesign@yandex.ru>
2018-08-08 20:51:54 +0700

    Stop and restart nginx on certs update.

--- webscaff/fabric/sys/certs.py
+++ webscaff/fabric/sys/certs.py
@@ -1,10 +1,13 @@
 from fabric.api import task, sudo
 
 from ..settings import PROJECT_NAME
+from .nginx import stop, restart
 
 __all__ = ['update']
 
 @task
 def update():
     """Updates SSL certificates using Let's Encrypt."""
+    stop()
     sudo('/home/%s/letsencrypt/letsencrypt-auto renew --agree-tos' % PROJECT_NAME)
+    restart()
